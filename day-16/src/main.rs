25ee8d1d9c0a70abda0e5283db0d47de60531847
Julien Faixo <julien@faixo.dev>
2023-12-17 12:35:37 +0100

    :sparkles: day 16

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,4 +1,191 @@
+use std::collections::HashSet;
+use std::io::stdin;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+enum BeamDirection {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct BeamState {
+    x: isize,
+    y: isize,
+    direction: BeamDirection,
+}
+
+enum CellKind {
+    Empty,
+    MirrorUp,
+    MirrorDown,
+    SplitterHorizontal,
+    SplitterVertical,
+}
+
+struct Statement {
+    mirror_map: Vec<Vec<CellKind>>,
+    width: usize,
+    height: usize,
+}
+
+struct Solver<'a> {
+    statement: &'a Statement,
+    heap: Vec<BeamState>,
+}
+
+impl<'a> Solver<'a> {
+    fn solve_part_2(&mut self) {
+        let mut energized_max = 0;
+
+        for x in 0..self.statement.width as isize {
+            let energized = self.send_beam(BeamState {
+                x,
+                y: 0,
+                direction: BeamDirection::Down,
+            });
+            energized_max = energized_max.max(energized);
+            let energized = self.send_beam(BeamState {
+                x,
+                y: self.statement.height as isize - 1,
+                direction: BeamDirection::Up,
+            });
+            energized_max = energized_max.max(energized);
+        }
+
+        for y in 0..self.statement.height as isize {
+            let energized = self.send_beam(BeamState {
+                x: 0,
+                y,
+                direction: BeamDirection::Right,
+            });
+            energized_max = energized_max.max(energized);
+            let energized = self.send_beam(BeamState {
+                x: self.statement.width as isize - 1,
+                y,
+                direction: BeamDirection::Left,
+            });
+            energized_max = energized_max.max(energized);
+        }
+
+        println!("{}", energized_max);
+    }
+
+    fn solve_part_1(&mut self) {
+        let energized = self.send_beam(BeamState {
+            x: 0,
+            y: 0,
+            direction: BeamDirection::Right,
+        });
+
+        println!("{}", energized);
+    }
+    fn send_beam(&mut self, initial_state: BeamState) -> u32 {
+        self.heap.clear();
+
+        let mut energized = vec![false; self.statement.width * self.statement.height];
+        self.heap.push(initial_state);
+
+        let mut already_visited =
+            HashSet::with_capacity(self.statement.width * self.statement.height);
+
+        while let Some(state) = self.heap.pop() {
+            if !already_visited.contains(&state) {
+                already_visited.insert(state);
+
+                energized[state.y as usize * self.statement.width + state.x as usize] = true;
+                self.move_beam(state.x, state.y, state.direction);
+            }
+        }
+
+        energized.iter().map(|v| *v as u32).sum()
+    }
+
+    fn move_beam(&mut self, x: isize, y: isize, direction: BeamDirection) {
+        match self.statement.mirror_map[y as usize][x as usize] {
+            CellKind::Empty => match direction {
+                BeamDirection::Up => self.add_to_heap(x, y - 1, direction),
+                BeamDirection::Right => self.add_to_heap(x + 1, y, direction),
+                BeamDirection::Down => self.add_to_heap(x, y + 1, direction),
+                BeamDirection::Left => self.add_to_heap(x - 1, y, direction),
+            },
+            CellKind::MirrorUp => match direction {
+                BeamDirection::Up => self.add_to_heap(x + 1, y, BeamDirection::Right),
+                BeamDirection::Right => self.add_to_heap(x, y - 1, BeamDirection::Up),
+                BeamDirection::Down => self.add_to_heap(x - 1, y, BeamDirection::Left),
+                BeamDirection::Left => self.add_to_heap(x, y + 1, BeamDirection::Down),
+            },
+            CellKind::MirrorDown => match direction {
+                BeamDirection::Up => self.add_to_heap(x - 1, y, BeamDirection::Left),
+                BeamDirection::Right => self.add_to_heap(x, y + 1, BeamDirection::Down),
+                BeamDirection::Down => self.add_to_heap(x + 1, y, BeamDirection::Right),
+                BeamDirection::Left => self.add_to_heap(x, y - 1, BeamDirection::Up),
+            },
+            CellKind::SplitterHorizontal => match direction {
+                BeamDirection::Right => self.add_to_heap(x + 1, y, BeamDirection::Right),
+                BeamDirection::Left => self.add_to_heap(x - 1, y, BeamDirection::Left),
+                BeamDirection::Up | BeamDirection::Down => {
+                    self.add_to_heap(x - 1, y, BeamDirection::Left);
+                    self.add_to_heap(x + 1, y, BeamDirection::Right);
+                }
+            },
+            CellKind::SplitterVertical => match direction {
+                BeamDirection::Up => self.add_to_heap(x, y - 1, BeamDirection::Up),
+                BeamDirection::Down => self.add_to_heap(x, y + 1, BeamDirection::Down),
+                BeamDirection::Right | BeamDirection::Left => {
+                    self.add_to_heap(x, y + 1, BeamDirection::Down);
+                    self.add_to_heap(x, y - 1, BeamDirection::Up);
+                }
+            },
+        }
+    }
+
+    fn add_to_heap(&mut self, x: isize, y: isize, direction: BeamDirection) {
+        if x >= 0
+            && x < self.statement.width as isize
+            && y >= 0
+            && y < self.statement.height as isize
+        {
+            self.heap.push(BeamState { x, y, direction });
+        }
+    }
+}
+
+fn parse_input() -> Statement {
+    let mirror_map: Vec<_> = stdin()
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+
+            line.chars()
+                .map(|c| match c {
+                    '/' => CellKind::MirrorUp,
+                    '\\' => CellKind::MirrorDown,
+                    '-' => CellKind::SplitterHorizontal,
+                    '|' => CellKind::SplitterVertical,
+                    _ => CellKind::Empty,
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let width = mirror_map[0].len();
+    let height = mirror_map.len();
+    Statement {
+        mirror_map,
+        width,
+        height,
+    }
+}
 
 fn main() {
+    let statement = parse_input();
+    let mut solver = Solver {
+        statement: &statement,
+        heap: Vec::with_capacity(100),
+    };
 
+    solver.solve_part_1();
+    solver.solve_part_2();
 }
--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,4 +1,177 @@
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashSet};
+use std::hash::{Hash, Hasher};
+use std::io::stdin;
 
+struct Statement {
+    width: usize,
+    height: usize,
+    map: Vec<Vec<usize>>,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    x: usize,
+    y: usize,
+    direction: usize,
+    straight_count: isize,
+}
+
+impl Hash for State {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+        self.direction.hash(state);
+    }
+}
+
+const DIRECTION: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Reversed for min heap
+        other
+            .cost
+            .cmp(&self.cost)
+            .then(self.straight_count.cmp(&other.straight_count))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn parse_input() -> Statement {
+    let map = stdin()
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            line.chars()
+                .map(|c| c as usize - '0' as usize)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let height = map.len();
+    let width = map[0].len();
+
+    Statement { width, height, map }
+}
 fn main() {
+    let statement = parse_input();
 
+    solve_part_1(&statement);
+    solve_part_2(&statement);
+}
+
+fn solve_part_1(statement: &Statement) {
+    let a = find_min_heat(
+        statement,
+        &State {
+            cost: 0,
+            x: 0,
+            y: 0,
+            direction: 5,
+            straight_count: 0,
+        },
+        1,
+        4,
+    );
+
+    eprintln!("{:?}", a);
+}
+
+fn solve_part_2(statement: &Statement) {
+    let a = find_min_heat(
+        statement,
+        &State {
+            cost: 0,
+            x: 0,
+            y: 0,
+            direction: 5,
+            straight_count: 0,
+        },
+        4,
+        11,
+    );
+
+    eprintln!("{:?}", a);
+}
+
+fn find_min_heat(
+    statement: &Statement,
+    initial_state: &State,
+    min_straight: isize,
+    max_straight: isize,
+) -> Option<usize> {
+    let mut costs = vec![usize::MAX; statement.width * statement.height * 4];
+    let mut visited = HashSet::new();
+    let mut heap = BinaryHeap::new();
+
+    costs[0] = 0;
+    costs[1] = 0;
+    costs[2] = 0;
+    costs[3] = 0;
+    heap.push(*initial_state);
+
+    while let Some(state) = heap.pop() {
+        if state.x == statement.width - 1 && state.y == statement.height - 1 {
+            return Some(state.cost);
+        }
+
+        if visited.contains(&state) {
+            continue;
+        }
+        visited.insert(state);
+
+        for dir_new in 0..4 {
+            if state.direction == dir_new || state.direction == (dir_new + 2) % 4 {
+                continue;
+            }
+            for d in min_straight..max_straight {
+                let y_new = state.y as isize + DIRECTION[dir_new].0 * d;
+                let x_new = state.x as isize + DIRECTION[dir_new].1 * d;
+
+                if x_new >= 0
+                    && x_new < statement.width as isize
+                    && y_new >= 0
+                    && y_new < statement.height as isize
+                {
+                    let y_new = y_new as usize;
+                    let x_new = x_new as usize;
+
+                    let mut cost_new = state.cost;
+                    if DIRECTION[dir_new].0 != 0 {
+                        for dy in 1..=d {
+                            cost_new += statement.map
+                                [(state.y as isize + DIRECTION[dir_new].0 * dy) as usize][state.x];
+                        }
+                    }
+                    if DIRECTION[dir_new].1 != 0 {
+                        for dx in 1..=d {
+                            cost_new += statement.map[state.y]
+                                [(state.x as isize + DIRECTION[dir_new].1 * dx) as usize];
+                        }
+                    }
+
+                    let next = State {
+                        cost: cost_new,
+                        x: x_new,
+                        y: y_new,
+                        direction: dir_new,
+                        straight_count: d,
+                    };
+
+                    if cost_new < costs[y_new * statement.width * 4 + x_new * 4 + dir_new] {
+                        heap.push(next);
+                        costs[y_new * statement.width * 4 + x_new * 4 + dir_new] = cost_new;
+                    }
+                }
+            }
+        }
+    }
+
+    None
 }
